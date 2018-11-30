135fe4c7c698605f935bd446cd64cb9633cdee52
EnricRG <EnricRG@users.noreply.github.com>
2018-11-30 22:35:52 +0100

    acabada documentada henraci

--- src/MapReduceActor.scala
+++ src/MapReduceActor.scala
@@ -1,7 +1,13 @@
 import akka.actor._
 
-/*
- * 
+/*	Class extending akka Actor containing an implementation of MapReduce design pattern. 
+ * 	To start the algorithm you have to instantiate an actor of this class, and then send him the message "start".
+ * 	Eventually it will compute and return to you the result.
+ * 	@param input List of pairs key value where the mapping function will be applied
+ * 	@param mapping Mapping function
+ * 	@param reducing Reduction function
+ * 	@param numMappers Number of actors that will be assigned to mapping operations
+ *  @param numReducers Number of actors that will be assigned to reducing operations
  */
 class MapReduceActor[K, V, K2, V2]
     (
@@ -11,9 +17,11 @@ class MapReduceActor[K, V, K2, V2]
     numMappers: Int,
     numReducers: Int                        ) extends Actor{
   
+  //master - service messages
   case class MapOrder(k: K, v: V, mapping: (K, V) => List[(K2, V2)])
   case class ReduceOrder(k: K2, vlist: List[V2], reducing: (K2, List[V2]) => List[V2])
   
+  //service - master messages
   case class Intermediate(list: List[(K2, V2)])
   case class Reduced(key: K2, list: List[V2]) 
   
@@ -34,16 +42,18 @@ class MapReduceActor[K, V, K2, V2]
   }
   
   def receive = {
+    //first call
     case "start" =>
       invoker = sender
       
-      if (input.length > 0) {
+      if (input.length > 0) { //if there's something to compute
       
         val groupSize = if (input.length/numMappers == 0) 1 else input.length/numMappers
         val groups = input.grouped(groupSize)
 
         var workers: List[akka.actor.ActorRef] = List()
         
+        //split the load between the actors
         for (group <- groups) {
           val worker = context.actorOf(Props(new Actor {
             def receive = {
@@ -53,13 +63,15 @@ class MapReduceActor[K, V, K2, V2]
           
           workers ::= worker
           
+          //order actors to map the pairs key value
           for ((key, value) <- group) worker ! MapOrder(key, value, mapping)
         }
         
         pendingIntermediates = input.length
       }
       else invoker ! Map()
-      
+     
+    //Between mapping and reducing
     case Intermediate(list) => 
       intermediates = list ::: intermediates
       pendingIntermediates -= 1
@@ -76,6 +88,7 @@ class MapReduceActor[K, V, K2, V2]
         val groupSize = if (dict.size/numMappers == 0) 1 else dict.size/numMappers
         val groups = dict.grouped(groupSize)
         
+        //split the load between the actors
         for (group <- groups){
           val worker = context.actorOf(Props(new Actor {
             def receive = {
@@ -85,18 +98,20 @@ class MapReduceActor[K, V, K2, V2]
           
           workers ::= worker
           
+          //order actors to reduce the values
           for ((key, values) <- group) worker ! ReduceOrder(key, values, reducing)
           
         } 
         
         pendingReduceds = dict.size
       }
-      
+    
+    //adding up the results
     case Reduced(key, values) =>
       result += (key -> values)
       pendingReduceds -= 1
       
-      if(pendingReduceds == 0)
+      if(pendingReduceds == 0) //if we have finished, send back to invoker the results
         
         invoker ! result
   }
